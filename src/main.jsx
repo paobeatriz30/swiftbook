55c39893146e2aeeb55042ed0b58876f1bce2cbf
paobeatriz30 <paobeatriz30@gmail.com>
2026-08-12 10:57:26 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SwiftBook from './SwiftBook.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SwiftBook />
+  </React.StrictMode>
+)
